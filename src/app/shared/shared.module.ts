8c90ea5c63a1ac817d6847dbae1f7dfc074a86a0
RUMOPC144\sigma <s.i.martynenko@gmail.com>
2018-11-27 15:23:37 +0300

    some header design

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,15 @@
-import {NgModule} from "@angular/core";
-import {CommonModule} from "@angular/common";
-import {HeaderComponent} from "./header/header.component";
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HeaderComponent} from './header/header.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule
   ],
   declarations: [
     HeaderComponent
